c788236210e81636dbe9d34bc3b380d8d3e54e16
gp <gp.globaliasoft@gmail.com>
2020-12-10 15:34:55 +0530

    changess

---
+++ build/precache-manifest.afe55a364343a8a8443d8006fd127679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da44102b00d1787d7858e3fb3f2a431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbdea4bfa0c68990c7d0",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "49ff55fa8149350ac4fd",
+    "url": "/static/js/2.1e21e911.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1e21e911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdea4bfa0c68990c7d0",
+    "url": "/static/js/main.82740a87.chunk.js"
+  },
+  {
+    "revision": "641871444896bdde3e7d",
+    "url": "/static/js/runtime-main.9debd4ba.js"
+  }
+]);
